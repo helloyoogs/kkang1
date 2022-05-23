c3c2e61b6e530fd12ef64337687c05f93a481b9a
helloyoogs <helloyoogs@naver.com>
2022-05-23 16:30:14 +0900

    Initial commit

---
+++ AndroidStudioProjects/AndroidLab/part2_5/src/main/java/com/example/part2_5/Lab5_2Activitty.java
@@ -0,0 +1,13 @@
+package com.example.part2_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab5_2Activitty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab5_2_activitty);
+    }
+}
